67461bd9d6632335b058c12e4a4fcc2d4e99fdf8
Jonas <publik.froeller.jonas@gmx.at>
2023-05-31 20:27:29 +0200

    test(vitest test:unit index.test.js): post-api-tests: auth(), postVideo(), signout()

--- frontend/src/index.test.js
+++ frontend/src/index.test.js
@@ -1,15 +1,12 @@
 import { describe, it, expect } from 'vitest';
+const axios = require('axios');
 const http = require('http');
 
-// API-Functions (with http because fetch (browser-function) is not NodeJS compatible)
-async function get(type, index, specification = '') {
-  const options = {
-    hostname: 'localhost',
-    port: 8196,
-    path: `/db_api.php?medium=${type}&id=${index}${specification !== '' ? '&medium_id=' + specification : ''}`,
-    method: 'GET'
-  };
+let jwt = {};
+let user = {};
+let accountExisted = false;
 
+function respond(options) {
   return new Promise((resolve, reject) => {
     const req = http.request(options, (res) => {
       let data = '';
@@ -29,6 +26,101 @@ async function get(type, index, specification = '') {
 
     req.end();
   });
+}
+
+// API-Functions (with http because fetch (browser-function) is not NodeJS compatible)
+/**
+ * @param {string} type
+ * @param {string} index (can be 'all' and 'random' too)
+ * @param {string} specification
+ */
+async function get(type, index, specification = '') {
+  const options = {
+    hostname: 'localhost',
+    port: 8196,
+    path: `/db_api.php?medium=${type}&id=${index}${specification !== '' ? '&medium_id=' + specification : ''}`,
+    method: 'GET'
+  };
+
+  return respond(options);
+}
+
+/**
+ * @param {string} username
+ * @param {string} password
+ */
+async function auth(username, password) {
+  const url = 'http://localhost:8196/db_api.php';
+  const formData = new FormData();
+  formData.append('action', 'auth');
+  formData.append('username', username);
+  formData.append('password', password);
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const response = await axios.post(url, formData, {
+      body: formData
+    });
+
+    if (response.status >= 200 && response.status <= 299) {
+			// console.log(response.text());
+			const json_response = await response.data;
+			// console.log(json_response);
+			jwt = json_response['token'];
+			accountExisted = JSON.parse(json_response['data'][0])[0]?.response == "accountExisted" ? true : false;
+			user = JSON.parse(json_response['data'][0])[0];
+			user.socials = JSON.parse(json_response['data'][0])[0]?.socials ?? null;
+			user.subscribed = JSON.parse(json_response['data'][0])[0]?.subscribed ?? null;
+			user.subscribers = JSON.parse(json_response['data'][0])[0]?.subscribers ?? null;
+			user.user_videos_liked = JSON.parse(json_response['data'][0])[0]?.liked ?? null;
+			user.user_videos_disliked = JSON.parse(json_response['data'][0])[0]?.disliked ?? null;
+			user.user_comments_liked =
+				JSON.parse(json_response['data'][0])[0]?.comments_liked ?? null;
+			user.user_comments_disliked =
+				JSON.parse(json_response['data'][0])[0]?.comments_disliked ?? null;
+			user.user_stats = JSON.parse(json_response['data'][0])[0]?.stats ?? null; // videos, likes, views, shares
+
+			return {
+				accountExisted: accountExisted,
+				token: jwt,
+				user: user
+			};
+		} else {
+			return { status: response.status, statusText: response.statusText };
+		}
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * @param {string} action
+ * @param {Record<string, any>} options
+ */
+async function post(action, options) {
+  const url = 'http://localhost:8196/db_api.php';
+  const formData = new FormData();
+  formData.append('action', action);
+  formData.append('HTTP_AUTHORIZATION', `Bearer ${jwt}`);
+
+  for (const [key, value] of options) {
+    formData.append(key, value);
+  }
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const response = await axios.post(url, formData, {
+      body: formData
+    });
+
+    if (response.status >= 200 && response.status <= 299) {
+      return await response.data;
+    } else {
+      return { status: response.status, statusText: response.statusText };
+    }
+  } catch (error) {
+    throw error;
+  }
 }
 
 // GET-API-Tests
@@ -137,5 +229,51 @@ describe('api-fetch-GET', () => {
     const response = await get('feedback', id);
     const result = JSON.parse(response['data']["feedback"]);
     expect(result.length).toBeGreaterThan(0);
+  })
+})
+
+// POST-API-Tests
+describe('api-fetch-POST', () => {
+  it('authenticate user', async () => {
+    const response = await auth('Jonesisfroellerix', "Password2$");
+    expect(response.token).toBeDefined();
+    expect(jwt).toBe(response.token);
+    expect(user).toBe(response["user"]);
+    expect(accountExisted).toBeTypeOf("boolean");
+  })
+  /**
+   * @vitest-environment jsdom
+   */
+  it('post video', async () => { // TODO: fix this test (file corrupts somehow)
+    const options = new Map();
+
+    const video = await fetch("http://localhost:8196/media/video/uploaded/vid_1.MP4");
+    const video_file = await video.blob();
+
+    const fileInput = document.createElement("input");
+    fileInput.type = 'file';
+
+    const fileData = {
+      name: 'test.mp4',
+      type: 'video/mp4',
+      size: video_file.size, 
+      lastModified: Date.now() 
+    };
+
+    const file = new Blob([video_file], fileData);
+    Object.defineProperty(fileInput, 'files', {
+      value: [file],
+      writable: false
+    });   
+
+    options.set("VIDEO_MEDIA", fileInput.files[0]);
+    options.set("VIDEO_TITLE", "Test Video");
+    options.set("VIDEO_DESCRIPTION", "Lorem ipsum dolor sit amet, consecte.");
+    const response = await post("video", options);
+    expect(response.token).toBe("valid");
+  })
+  it('signout user', async () => {
+    const response = await post("signout", []);
+    expect(response.token).toBe("unset");
   })
 })
